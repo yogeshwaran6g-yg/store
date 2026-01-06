12c947a02bf9f88be79fb25038de9ea267ed0cb1
Arunvel2004 <arunv4995@gmail.com>
2026-01-06 10:12:44 +0530

    deleted dist and inputarea bug fix

--- client/src/components/form/InputArea.jsx
+++ client/src/components/form/InputArea.jsx
@@ -1,4 +1,4 @@
-import Label from "./lLbel";
+import Label from "./Label";
 const InputArea = ({
   name,
   label,
@@ -34,11 +34,9 @@ const InputArea = ({
           placeholder={placeholder}
           autoComplete={autocomplete}
           maxLength={maxLength}
-            className={`${
-            Icon ? "py-2 pl-10" : "py-2 px-4 md:px-5"
-          } w-full appearance-none border text-sm opacity-75 text-input rounded-md placeholder-body min-h-12 transition duration-200 focus:ring-0 ease-in-out bg-white border-gray-200 focus:outline-none focus:border-emerald-500 h-11 md:h-12 ${
-            readOnly ? "bg-gray-100 cursor-not-allowed text-gray-500" : ""
-          }`}
+          className={`${Icon ? "py-2 pl-10" : "py-2 px-4 md:px-5"
+            } w-full appearance-none border text-sm opacity-75 text-input rounded-md placeholder-body min-h-12 transition duration-200 focus:ring-0 ease-in-out bg-white border-gray-200 focus:outline-none focus:border-emerald-500 h-11 md:h-12 ${readOnly ? "bg-gray-100 cursor-not-allowed text-gray-500" : ""
+            }`}
         />
       </div>
     </>
